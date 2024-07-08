fc9c91ec9273551bea2971d7a7461a5e3224f6ec
Kate <kate.burovova@gmail.com>
2024-07-08 16:05:15 +0300

    add exception

--- pages/page1.py
+++ pages/page1.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-if st.session_state.logged_in:
-    st.write('Hi')
-else:
-    st.write('User not recognised')
+try:
+    if st.session_state.logged_in:
+        st.write('Hi')
+    else:
+        st.write('User not recognised')
+except Exception as e:
+    "You are not authorised to visit this page"
