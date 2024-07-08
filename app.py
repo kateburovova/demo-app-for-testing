fb77820d567dfd949d436c293f53fb984a1e8b84
Kate <kate.burovova@gmail.com>
2024-07-08 15:44:35 +0300

    fix auth

--- app.py
+++ app.py
@@ -12,11 +12,9 @@ email = st.text_input("Enter email")
 password = st.text_input("Enter password", type="password")
 
 if st.button("Log in", type="primary"):
-
     try:
         st_supabase_client.auth.sign_in_with_password(dict(email=email, password=password))
         st.session_state.logged_in = True
         st.success(f"Logged in as {email}")
-        st.switch_page("pages/page1.py")
     except Exception as e:
         st.error(f"There is an issue with your credentials: ({e}).\nPlease try again.")
