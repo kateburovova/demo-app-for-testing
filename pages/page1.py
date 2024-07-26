390eb7624619b9567e20cb8a8199df0c224f3aa8
Kate <kate.burovova@gmail.com>
2024-07-26 15:33:37 +0300

    add test dashboard

--- pages/page1.py
+++ pages/page1.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 try:
     if st.session_state.logged_in:
         st.write('Hi')
+        kibana_url = st.secrets['KIBANA_DASHBOARD_URL']
+        components.iframe(kibana_url, height=600, scrolling=True)
     else:
         st.write('User not recognised')
 except Exception as e:
     "You are not authorised to visit this page"
+    
+
